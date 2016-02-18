446d2b461ff1e0c9dad9f7001ad83344405ab0b6
Andrew Cutler <andrew@panubo.com>
2016-02-18 13:49:55 +1100

    Made generic

--- check-images.sh
+++ check-images.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 IMAGES=$(docker images | awk '{print $1":"$2}' | egrep -v 'REPOSITORY|<none>' | sort -u )
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for I in $IMAGES; do
     echo -n "$I - "
-    ID=$(docker run -d --entrypoint=/bin/sh -v /mnt:/mnt $I /mnt/wait.sh)
-    EXEC=$(docker exec -it $ID sh -c "/mnt/cve-2015-7547.sh" 2>/dev/null)
-    if [ "$?" -ne "0" ]; then
-        echo "Container FAILED"
-    else
-        echo $EXEC
-    fi
+    ID=$(docker run -d --entrypoint=/bin/sh -v ${DIR}:${DIR} $I ${DIR}/wait.sh)
+
+    for C in $DIR/checks/*.sh; do
+        EXEC=$(docker exec -it $ID sh -c "$C" 2>/dev/null)
+        if [ "$?" -ne "0" ]; then
+            echo "Container FAILED"
+        else
+            echo "$EXEC"
+        fi
+    done
     docker rm -f $ID 1> /dev/null
 done
